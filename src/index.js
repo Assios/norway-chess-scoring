8485c8736191d025015d8e8e041a343bf8080307
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-06-07 23:38:41 +0200

    Add files

---
+++ src/index.js
@@ -0,0 +1,111 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Table from "./table.js";
+import data from "./data/results.json";
+
+import "./styles.css";
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { players: [], w: 2, d: 0.5, l: 0, aw: 1, al: 0 };
+
+    this.handleClassicalWinChange = this.handleClassicalWinChange.bind(this);
+    this.handleArmageddonWinChange = this.handleArmageddonWinChange.bind(this);
+    this.handleClassicalDrawChange = this.handleClassicalDrawChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({players: data.players});
+  }
+
+  handleClassicalWinChange(event) {
+    this.setState({ w: event.target.value });
+  }
+
+  handleClassicalDrawChange(event) {
+    this.setState({ d: event.target.value });
+  }
+
+  handleArmageddonWinChange(event) {
+    this.setState({ aw: event.target.value });
+  }
+
+  handleSelectChange(event) {
+    switch(event.target.value) {
+      case "nc":
+        this.setState({ w: 2, d: 0.5, l: 0, aw: 1, al: 0 });
+        break;
+      case "regular":
+        this.setState({ w: 1, d: 0.5, l: 0, aw: 0, al: 0 });
+        break;
+      case "hockey":
+        this.setState({ w: 3, d: 1, l: 0, aw: 1, al: 0 });
+        break;
+      case "4210":
+        this.setState({ w: 4, d: 1, l: 0, aw: 1, al: 0 });
+    }
+  }
+
+  render() {
+    const p = this.state.players.map(function(player) {
+      return {
+        ...player,
+        score:
+        player.classicalWins * this.state.w +
+        player.classicalDraws * this.state.d +
+        player.classicalLosses * this.state.l +
+        player.armageddonWins * this.state.aw +
+        player.armageddonLosses * this.state.al
+      };
+    }, this).
+    sort((a, b) => b.score - a.score);
+
+    return (
+      <div className="wrapper">
+        <h2>Norway Chess 2019</h2>
+        <div className="form-inline">
+          <div className="input-group mb-4 mr-sm-4">
+            <div className="input-group-prepend">
+              <div className="input-group-text">Points for classical win</div>
+            </div>
+            <input type="number" className="form-control" value={this.state.w} onChange={this.handleClassicalWinChange} />
+          </div>
+
+          <div className="input-group mb-4 mr-sm-4">
+            <div className="input-group-prepend">
+              <div className="input-group-text">Points for classical draw</div>
+            </div>
+            <input type="number" className="form-control" value={this.state.d} onChange={this.handleClassicalDrawChange} />
+          </div>
+
+          <div className="input-group mb-4 mr-sm-4">
+            <div className="input-group-prepend">
+              <div className="input-group-text">Points for Armageddon win</div>
+            </div>
+            <input type="number" className="form-control" value={this.state.aw} onChange={this.handleArmageddonWinChange} />
+          </div>
+        </div>
+
+        <div className="form-group row">
+          <label className="col-sm-1 col-form-label"><b>Preset</b></label>
+          <div className="col-sm-4">
+            <select className="form-control mb-2 mr-sm-2" onChange={this.handleSelectChange}>
+              <option selected value="nc">Norway Chess</option>
+              <option value="regular">Regular</option>
+              <option value="hockey">Hockey</option>
+              <option value="4210">4-2-1-0</option>
+            </select>
+          </div>
+        </div>
+
+
+        <Table players={p} />
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
---
+++ src/table.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const TableRow = ({ row, rowNumber }) => (
+  <tr>
+    <th scope="row">{rowNumber}</th>
+    <td>{row.name}</td>
+    <td><b>{row.score}</b></td>
+    <td>{row.classicalWins}</td>
+    <td>{row.classicalDraws}</td>
+    <td>{row.classicalLosses}</td>
+    <td>{row.armageddonWins}</td>
+    <td>{row.armageddonLosses}</td>
+  </tr>
+);
+
+class Table extends React.Component {
+  render() {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Score</th>
+            <th scope="col" title="Classical wins">W</th>
+            <th scope="col" title="Classical draws">D</th>
+            <th scope="col" title="Classical losses">L</th>
+            <th scope="col" title="Armageddon wins">AW</th>
+            <th scope="col" title="Armageddon losses">AL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.players.map((item, key) => (
+            <TableRow row={item} rowNumber={key + 1} />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+export default Table;
